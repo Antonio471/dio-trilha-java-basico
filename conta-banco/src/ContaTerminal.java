84b7d82179f1eece010fa4242a87d34c7b3ae3db
Antonio <antonio.carlos.48slz@gmail.com>
2025-03-23 09:40:33 -0300

    conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,26 +6,22 @@ public class ContaTerminal {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int numero = 0;
-        String agencia = "";
-        String nomeCliente = "";
-        double saldo = 0.0;
-
         System.out.println("Por favor, digite o número da conta!");
-        sc.nextInt();
-
-        System.out.println("Digite o número da agência!");
-        sc.nextInt();
-
-        System.out.println("Informe o nome do cliente.");
+        int numero = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Informe o saldo da conta.");
-        sc.nextDouble();
+        System.out.println("Digite o número da agência!");
+        String agencia = sc.nextLine();
 
-        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta" 
-        + "nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo" 
-        + saldo + "já está disponível para saque.");
+        System.out.println("Informe o nome do cliente.");
+        String nomeCliente = sc.nextLine();
+
+
+        System.out.println("Informe o saldo da conta.");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta!%n", nomeCliente); 
+        System.out.printf("Sua agência é %s, conta %d e seu saldo %.2f já disponível para saque.%n", agencia, numero, saldo);
 
         sc.close();
     }
